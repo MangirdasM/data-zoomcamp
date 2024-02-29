87011f86477cc9d6512c74d1fc63eb722f4e5d9c
Mangirdas Meškauskas <mangirdas.mesk@gmail.com>
2024-02-29 11:10:57 +0000

    Learning dbt

--- 04-analytics-engineering/taxi_rides_ny_new/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny_new/models/staging/stg_yellow_tripdata.sql
@@ -3,10 +3,10 @@
 select 
 -- identifiers
     {{dbt_utils.surrogate_key(['vendor_id', 'pickup_datetime'])}} as tripid,
-    cast(vendor_id as integer ) as vendorid,
-    cast(rate_code as integer ) as ratecodeid,
-    cast(pickup_location_id as integer ) as pickup_locationid,
-    cast(dropoff_location_id as integer ) as dropoff_locationid,
+    {{ dbt.safe_cast("vendor_id", api.Column.translate_type("integer")) }} as vendorid,
+    {{ dbt.safe_cast("rate_code", api.Column.translate_type("integer")) }} as ratecodeid,
+    {{ dbt.safe_cast("pickup_location_id", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("dropoff_location_id", api.Column.translate_type("integer")) }} as dropoff_locationid,
     
     -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
@@ -14,8 +14,10 @@ select
     
     -- trip info
     store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
+    {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }} as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
 
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -23,6 +25,7 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
+    cast(0 as numeric) as ehail_fee,
     cast(imp_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce({{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }},0) as payment_type,
